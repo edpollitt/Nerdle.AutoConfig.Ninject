5d4ed1f61ea0558851a8e207e363b0b8df57a88b
edpollitt <edpollitt@gmail.com>
2015-10-30 13:48:45 +0000

    initial commit

---
+++ Nerdle.AutoConfig.Ninject/AutoConfigNinjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Ninject.Syntax;
+
+namespace Nerdle.AutoConfig.Ninject
+{
+    public static class AutoConfigNinjectExtensions
+    {
+        public static IBindingWhenInNamedWithOrOnSyntax<T> ToConfig<T>(this IBindingToSyntax<T> bind, string sectionName = null)
+        {
+            var config = AutoConfig.Map<T>(sectionName);
+            return bind.ToConstant(config);
+        }
+    }
+}
