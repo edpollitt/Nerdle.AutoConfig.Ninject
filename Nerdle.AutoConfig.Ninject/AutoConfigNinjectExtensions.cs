97b280e775bcd26709d04a571836ca9b25528e58
edpollitt <edpollitt@gmail.com>
2015-11-02 17:27:13 +0000

    Changed binding to transient by default

--- Nerdle.AutoConfig.Ninject/AutoConfigNinjectExtensions.cs
+++ Nerdle.AutoConfig.Ninject/AutoConfigNinjectExtensions.cs
@@ -6,8 +6,7 @@ namespace Nerdle.AutoConfig.Ninject
     {
         public static IBindingWhenInNamedWithOrOnSyntax<T> ToConfig<T>(this IBindingToSyntax<T> bind, string sectionName = null)
         {
-            var config = AutoConfig.Map<T>(sectionName);
-            return bind.ToConstant(config);
+            return bind.ToMethod(ctx => AutoConfig.Map<T>(sectionName));
         }
     }
 }
